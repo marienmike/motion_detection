271e807574a66d63559186aa9e9e7d2a91765409
marienmike <marien.come@gmail.com>
2019-05-03 15:25:40 +0000

    Add files via upload

---
+++ extract_img.py
@@ -0,0 +1,12 @@
+import cv2
+vidcap = cv2.VideoCapture('conv.mp4')
+print("Video captured")
+success,image = vidcap.read()
+count = 0
+while success:
+  cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file      
+  success,image = vidcap.read()
+  print('Read a new frame: ', success)
+  count += 1
+
+
